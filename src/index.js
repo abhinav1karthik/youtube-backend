b21809ac17f00d91a5d6c8fad251f3f1cf5675c5
Abhinav Karthik <abhinavkarthik.prattipati@gmail.com>
2025-09-30 20:37:41 +0530

    Fix deployment configuration for Vercel

--- src/index.js
+++ src/index.js
@@ -2,9 +2,7 @@ import dotenv from 'dotenv'
 import connectToDB from './db/dbConnect.js'
 import app from './app.js'
 
-dotenv.config({
-    path: '/env'  
-})
+dotenv.config()
 
 connectToDB()
 .then(() => {
